2cb949203de7e8e0ce22ebd406ced40e79d0aa29
Segma02 <64342026+Segma02@users.noreply.github.com>
2020-05-14 18:46:18 +0000

    base de données
    Ce dossier contient la base de données Mysql de l'application

---
+++ database/emploi.sql
@@ -0,0 +1,281 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le :  jeu. 14 mai 2020 à 20:43
+-- Version du serveur :  10.4.11-MariaDB
+-- Version de PHP :  7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `emploi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `candidat`
+--
+
+CREATE TABLE `candidat` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `prenom` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `pays` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `adresse` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `photo` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `creation` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cv`
+--
+
+CREATE TABLE `cv` (
+  `id` int(11) NOT NULL,
+  `candidat_id` int(11) DEFAULT NULL,
+  `nom` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `prenom` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `profession` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `photo` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `lieu` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `tel` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `adresse` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `competences` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `experience` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `languages` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `projets` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `date` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `destinataire` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `accepted` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `dateValidation` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `entreprise`
+--
+
+CREATE TABLE `entreprise` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `pays` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `adresse` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `categorie` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `photo` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `secteur` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `statut` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `taille` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `creation` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `favoris`
+--
+
+CREATE TABLE `favoris` (
+  `id` int(11) NOT NULL,
+  `candidat_id` int(11) DEFAULT NULL,
+  `nom` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `adresse` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `departement` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `lieu` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `contrat` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `poste` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `date` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `photo` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `notificationa`
+--
+
+CREATE TABLE `notificationa` (
+  `id` int(11) NOT NULL,
+  `offre_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `notificationb`
+--
+
+CREATE TABLE `notificationb` (
+  `id` int(11) NOT NULL,
+  `cv_id` int(11) DEFAULT NULL,
+  `destination` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `offre`
+--
+
+CREATE TABLE `offre` (
+  `id` int(11) NOT NULL,
+  `entreprise_id` int(11) DEFAULT NULL,
+  `nom` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `adresse` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `departement` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `lieu` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `contrat` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `poste` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `date` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `photo` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `candidat`
+--
+ALTER TABLE `candidat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `cv`
+--
+ALTER TABLE `cv`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_B66FFE928D0EB82` (`candidat_id`);
+
+--
+-- Index pour la table `entreprise`
+--
+ALTER TABLE `entreprise`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `favoris`
+--
+ALTER TABLE `favoris`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_8933C4328D0EB82` (`candidat_id`);
+
+--
+-- Index pour la table `notificationa`
+--
+ALTER TABLE `notificationa`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_93B9C19B4CC8505A` (`offre_id`);
+
+--
+-- Index pour la table `notificationb`
+--
+ALTER TABLE `notificationb`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_AB09021CFE419E2` (`cv_id`);
+
+--
+-- Index pour la table `offre`
+--
+ALTER TABLE `offre`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_AF86866FA4AEAFEA` (`entreprise_id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `candidat`
+--
+ALTER TABLE `candidat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+
+--
+-- AUTO_INCREMENT pour la table `cv`
+--
+ALTER TABLE `cv`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
+
+--
+-- AUTO_INCREMENT pour la table `entreprise`
+--
+ALTER TABLE `entreprise`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT pour la table `favoris`
+--
+ALTER TABLE `favoris`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+
+--
+-- AUTO_INCREMENT pour la table `notificationa`
+--
+ALTER TABLE `notificationa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT pour la table `notificationb`
+--
+ALTER TABLE `notificationb`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+
+--
+-- AUTO_INCREMENT pour la table `offre`
+--
+ALTER TABLE `offre`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `cv`
+--
+ALTER TABLE `cv`
+  ADD CONSTRAINT `FK_B66FFE928D0EB82` FOREIGN KEY (`candidat_id`) REFERENCES `candidat` (`id`);
+
+--
+-- Contraintes pour la table `favoris`
+--
+ALTER TABLE `favoris`
+  ADD CONSTRAINT `FK_8933C4328D0EB82` FOREIGN KEY (`candidat_id`) REFERENCES `candidat` (`id`);
+
+--
+-- Contraintes pour la table `notificationa`
+--
+ALTER TABLE `notificationa`
+  ADD CONSTRAINT `FK_93B9C19B4CC8505A` FOREIGN KEY (`offre_id`) REFERENCES `offre` (`id`);
+
+--
+-- Contraintes pour la table `notificationb`
+--
+ALTER TABLE `notificationb`
+  ADD CONSTRAINT `FK_AB09021CFE419E2` FOREIGN KEY (`cv_id`) REFERENCES `cv` (`id`);
+
+--
+-- Contraintes pour la table `offre`
+--
+ALTER TABLE `offre`
+  ADD CONSTRAINT `FK_AF86866FA4AEAFEA` FOREIGN KEY (`entreprise_id`) REFERENCES `entreprise` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
